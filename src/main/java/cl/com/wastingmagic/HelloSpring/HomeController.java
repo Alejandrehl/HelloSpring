1a55287dccfb652948353ea40022749fc4df48e1
Alejandro Exequiel Hernández Lara <Alejandrehl@icloud.com>
2020-03-03 17:14:32 -0300

    Home Controller & Readme

---
+++ src/main/java/cl/com/wastingmagic/HelloSpring/HomeController.java
@@ -0,0 +1,13 @@
+package cl.com.wastingmagic.HelloSpring;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String index() {
+        return "Spring Hello World";
+    }
+}
