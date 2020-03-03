3569c3777b76b9a4dfc0491050ab56b9b00b088f
Alejandro Exequiel Hernández Lara <Alejandrehl@icloud.com>
2020-03-03 18:18:39 -0300

    Hello world with thymeleaf done

--- src/main/java/cl/com/wastingmagic/HelloSpring/HomeController.java
+++ src/main/java/cl/com/wastingmagic/HelloSpring/HomeController.java
@@ -1,16 +1,13 @@
 package cl.com.wastingmagic.HelloSpring;
 
-import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@Slf4j
+@Controller
 public class HomeController {
 
     @GetMapping("/")
     public String index() {
-        log.info("Ejecutando el controlador rest");
-        return "Spring Hello World";
+        return "index";
     }
 }
