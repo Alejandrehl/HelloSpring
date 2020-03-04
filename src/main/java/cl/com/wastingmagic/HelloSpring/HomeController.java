4c69e68cdc403c8cd1bdc522b9ab56545f863e32
Alejandro Exequiel Hernández Lara <Alejandrehl@icloud.com>
2020-03-04 18:34:10 -0300

    Seteando y mostrando atributos de un modelo en un controlador

--- src/main/java/cl/com/wastingmagic/HelloSpring/HomeController.java
+++ src/main/java/cl/com/wastingmagic/HelloSpring/HomeController.java
@@ -1,5 +1,6 @@
 package cl.com.wastingmagic.HelloSpring;
 
+import cl.com.wastingmagic.HelloSpring.domain.Person;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -13,8 +14,17 @@ public class HomeController {
     @GetMapping("/")
     public String index(Model model) {
         var message = "Ultimate Spring Hello World";
+        var person = new Person();
+
+        person.setName("Alejandro");
+        person.setLastName("Hernández");
+        person.setEmail("+56951064707");
+        person.setEmail("Alejandrehl@icloud.com");
+
         model.addAttribute("message", message);
         model.addAttribute("greeting", greeting);
+        model.addAttribute("person", person);
+
         return "index";
     }
 }
---
+++ src/main/java/cl/com/wastingmagic/HelloSpring/domain/Person.java
@@ -0,0 +1,11 @@
+package cl.com.wastingmagic.HelloSpring.domain;
+
+import lombok.Data;
+
+@Data
+public class Person {
+    private String name;
+    private String lastName;
+    private String email;
+    private String phone;
+}
