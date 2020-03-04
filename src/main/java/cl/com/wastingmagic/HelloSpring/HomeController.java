3c391ff03c25ad447e98f36f25aa8f530cfa4b07
Alejandro Exequiel Hernández Lara <Alejandrehl@icloud.com>
2020-03-04 18:19:07 -0300

    Pasando valores a la vista por medio del controlador desde el application.properties

--- src/main/java/cl/com/wastingmagic/HelloSpring/HomeController.java
+++ src/main/java/cl/com/wastingmagic/HelloSpring/HomeController.java
@@ -1,16 +1,20 @@
 package cl.com.wastingmagic.HelloSpring;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
+    @Value("${index.greetings}")
+    private String greeting;
 
     @GetMapping("/")
     public String index(Model model) {
         var message = "Ultimate Spring Hello World";
         model.addAttribute("message", message);
+        model.addAttribute("greeting", greeting);
         return "index";
     }
 }
