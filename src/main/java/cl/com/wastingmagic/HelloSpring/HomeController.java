08f268665889645ea2a430ded139a39a0584cff1
Alejandro Exequiel Hernández Lara <Alejandrehl@icloud.com>
2020-03-04 10:44:25 -0300

    save

--- src/main/java/cl/com/wastingmagic/HelloSpring/HomeController.java
+++ src/main/java/cl/com/wastingmagic/HelloSpring/HomeController.java
@@ -1,13 +1,16 @@
 package cl.com.wastingmagic.HelloSpring;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
 
     @GetMapping("/")
-    public String index() {
+    public String index(Model model) {
+        var message = "Ultimate Spring Hello World";
+        model.addAttribute("message", message);
         return "index";
     }
 }
